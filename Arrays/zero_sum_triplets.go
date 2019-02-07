31170fe70a1f52d03a48af063da44fc7c36e5410
Aayush Sarva <checkaayush@gmail.com>
2019-02-08 01:13:53 +0530

    Optimize zero sum triplets

--- Arrays/zero_sum_triplets.go
+++ Arrays/zero_sum_triplets.go
@@ -8,15 +8,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+// main Driver
 func main() {
 	array := takeUserInput()
-	findZeroSumTriplets(array)
+	printZeroSumTripletsV3(array)
 }
 
+// takeUserInput ...
 func takeUserInput() []int {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter array of integers: ")
@@ -37,12 +40,64 @@ func takeUserInput() []int {
 	return integers
 }
 
-func findZeroSumTriplets(array []int) {
+// printZeroSumTripletsV3 O(n^2) time, O(1) space
+func printZeroSumTripletsV3(array []int) {
 	found := false
-	arrayLen := len(array)
-	for i := 0; i < arrayLen-2; i++ {
-		for j := i + 1; j < arrayLen-1; j++ {
-			for k := j + 1; k < arrayLen; k++ {
+	n := len(array)
+
+	sort.Ints(array)
+
+	for i := 0; i < n-2; i++ {
+		l, r := i+1, n-1
+		for l < r {
+			sum := array[i] + array[l] + array[r]
+			if sum == 0 {
+				l++
+				r--
+				fmt.Println(array[i], array[l], array[r])
+				found = true
+			} else if sum < 0 {
+				l++
+			} else {
+				r--
+			}
+		}
+	}
+
+	if found == false {
+		fmt.Println("No zero sum triplets found")
+	}
+}
+
+// printZeroSumTripletsV2 O(n^2) time, O(n) space
+func printZeroSumTripletsV2(array []int) {
+	found := false
+	n := len(array)
+	for i := 0; i < n-2; i++ {
+		m := make(map[int]bool)
+		for j := i + 1; j < n-1; j++ {
+			k := -(array[i] + array[j])
+			if _, exists := m[k]; exists {
+				fmt.Println(array[i], array[j], k)
+				found = true
+			} else {
+				m[array[j]] = true
+			}
+		}
+	}
+
+	if found == false {
+		fmt.Println("No zero sum triplets found")
+	}
+}
+
+// printZeroSumTripletsV1 O(n^3) time, O(1) space
+func printZeroSumTripletsV1(array []int) {
+	found := false
+	n := len(array)
+	for i := 0; i < n-2; i++ {
+		for j := i + 1; j < n-1; j++ {
+			for k := j + 1; k < n; k++ {
 				if array[i]+array[j]+array[k] == 0 {
 					fmt.Println(array[i], array[j], array[k])
 					found = true
@@ -55,4 +110,3 @@ func findZeroSumTriplets(array []int) {
 		fmt.Println("No zero sum triplets found")
 	}
 }
-
